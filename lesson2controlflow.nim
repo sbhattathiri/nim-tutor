45afa671a861009eb37bb9fe63c21adb487fd128
surajvbhattathiri <surajv.bhattathiri@nielsen.com>
2021-03-27 12:01:51 +0530

    when

--- lesson2controlflow.nim
+++ lesson2controlflow.nim
@@ -29,3 +29,19 @@ of 0..2, 4..7:
 of 3, 8: 
   echo "The number is 3 or 8"
 else: discard
+
+# when
+#[
+  The when statement is almost identical to the if statement, but with these differences:
+  - Each condition must be a constant expression since it is evaluated by the compiler.
+  - The statements within a branch do not open a new scope.
+  - The compiler checks the semantics and produces code only for the statements that belong to the first condition that evaluates to true.
+]#
+when system.hostOS == "windows":
+  echo "running on Windows!"
+elif system.hostOS == "linux":
+  echo "running on Linux!"
+elif system.hostOS == "macosx":
+  echo "running on Mac OS X!"
+else:
+  echo "unknown operating system"
---
+++ scopes.nim
@@ -0,0 +1,13 @@
+var x:int = 10
+echo x
+
+var y:int = 100
+
+# explicit block
+# The block's label (myBlock in the example) is optional
+block myBlock:
+    var y:int = 1
+    echo y
+
+echo y
+    
